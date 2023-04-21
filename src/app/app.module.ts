75edd6bda815b8a2523b6f946266d29669895d6d
PrudhviBharadwajM <prudhvi.maale@gmail.com>
2023-04-21 18:37:54 -0400

    initial commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+
+    //Material Design
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private fireauth: AngularFireAuth, private router: Router) { }
+
+  //login
+  login(email: string, password: string) {
+    this.fireauth.signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        localStorage.setItem('token', 'true');
+        this.router.navigate(['/dashboard']);
+      }, err =>{
+        alert('Something went wrong');
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  //register
+  register(email: string, password: string) {
+    this.fireauth.createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        alert('Registration successful');
+        this.router.navigate(['/login']);
+      }, err =>{
+        alert('Something went wrong');
+        this.router.navigate(['/register']);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  //logout
+  logout() {
+    this.fireauth.signOut().then(() => {
+      localStorage.removeItem('token');
+      this.router.navigate(['/login']);
+    }, err =>{
+      alert(err.message);
+    });
+  }
+
+}
