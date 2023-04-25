d1d5fe86d650ed25e87d6126785ef106da7c8c45
PrudhviBharadwajM <prudhvi.maale@gmail.com>
2023-04-25 12:16:31 -0400

    rolling the changes back to use davinci GPT model.

--- src/app/shared/open-ai.service.ts
+++ src/app/shared/open-ai.service.ts
@@ -18,12 +18,17 @@ export class OpenAiService {
   public openai = new OpenAIApi(this.configuration);
 
   async getDataFromOpenAPI(text: string) {
-    const completion = await this.openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{role: "user", content: text}],
+    const completion = await this.openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: text,
+      temperature: 0.7,
+      max_tokens: 3800,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
-    
-    const response = completion.data.choices[0].message?.content;
-    return response;
+
+  const response = completion.data.choices[0].text;
+  return response;
 }
 }
